d4d7c840e144888567260f4ac23f07928829e0d9
Daven <daps2182gmail.com>
2021-11-01 13:13:32 +0300

    ropsten

--- packages/hardhat/scripts/mint.js
+++ packages/hardhat/scripts/mint.js
@@ -12,7 +12,7 @@ const delayMS = 1000 //sometimes xDAI needs a 6000ms break lol 😅
 const main = async () => {
 
   // ADDRESS TO MINT TO:
-  const toAddress = "0x34aA3F359A9D614239015126635CE7732c18fDF3"
+  const toAddress = "0xAcc2E54E1481b9130d8F18F7e3C59c36a7F1A53d"
 
   console.log("\n\n 🎫 Minting to "+toAddress+"...\n");
 
@@ -210,11 +210,8 @@ const main = async () => {
   await sleep(delayMS)
 
   /*
-
-
   console.log("Minting zebra...")
   await yourCollectible.mintItem("0xD75b0609ed51307E13bae0F9394b5f63A7f8b6A1","zebra.jpg")
-
   */
 
 
